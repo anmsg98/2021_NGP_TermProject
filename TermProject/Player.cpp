472c934fc9a430e33938d3e19575e7b2d2d8ebe1
anmsg98 <wjstjd1004@naver.com>
2021-11-06 23:57:30 +0900

    [코드 추가] 마우스 좌표에따른 총알 발사 구현 및 구현을 위한 CBullet 클래스 멤버변수 및 함수 추가

--- TermProject/Player.cpp
+++ TermProject/Player.cpp
@@ -7,9 +7,10 @@ void CBullet::Update(float DeltaTime)
 {
 	if (m_IsActive)
 	{
-		m_Position.m_X += 300.0f * DeltaTime;
+		m_Position.m_X += (GetDirect().x / GetLength()) * DeltaTime * 1000.f;
+		m_Position.m_Y += (GetDirect().y / GetLength()) * DeltaTime * 1000.f;
 
-		if (m_Position.m_X >= 2000)
+		if (m_Position.m_X >= 2000 || m_Position.m_X < 0 || m_Position.m_Y >= 1000 || m_Position.m_Y < 0)
 		{
 			SetActive(false);
 		}
@@ -47,19 +48,21 @@ CBullet* CPlayer::GetBullets()
 	return m_Bullets;
 }
 
-void CPlayer::FireBullet()
+void CPlayer::FireBullet(int mx, int my)
 {
 	if (m_Bullets)
 	{
 		m_Bullets[m_BulletIndex].SetActive(true);
-		m_Bullets[m_BulletIndex].SetPosition(CPlayer::GetPosition().m_X, CPlayer::GetPosition().m_Y);
-
+		m_Bullets[m_BulletIndex].SetPosition(GetPosition().m_X, GetPosition().m_Y);
+		m_Bullets[m_BulletIndex].SetDirect(mx + GetCameraStartPosition().x - GetPosition().m_X, my + GetCameraStartPosition().y - GetPosition().m_Y);
+		m_Bullets[m_BulletIndex].SetLength(sqrt(pow(m_Bullets[m_BulletIndex].GetDirect().x, 2) + pow(m_Bullets[m_BulletIndex].GetDirect().y, 2)));
 		m_BulletIndex = (m_BulletIndex + 1) % MAX_BULLET;
 	}
 }
 
 void CPlayer::Animate(float DeltaTime)
 {
+	printf("%.2f, %.2f\r", GetPosition().m_X, GetPosition().m_Y);
 	for (int i = 0; i < MAX_BULLET; ++i)
 	{
 		m_Bullets[i].Update(DeltaTime);
@@ -69,6 +72,7 @@ void CPlayer::Animate(float DeltaTime)
 void CPlayer::UpdateCamera(const RECT& ClientRect, const RECT& MapRect)
 {
 	// 맵의 구석으로 가면 더이상 움직이지 않는다.
+	
 	m_CameraStartPosition.x = m_Position.m_X - (ClientRect.right / 2);
 	m_CameraStartPosition.y = m_Position.m_Y - (ClientRect.bottom / 2);
 
--- TermProject/Player.h
+++ TermProject/Player.h
@@ -7,12 +7,18 @@
 
 class CBullet : public CGameObject
 {
+private:
+	float m_LineLength;
+	POINT m_Dir;
 public:
 	CBullet() = default;
 	~CBullet() = default;
-
 	virtual void Update(float DeltaTime);
-};
+	void SetLength(float length) { m_LineLength = length; }
+	float GetLength() { return m_LineLength; }
+	void SetDirect(float dirx, float diry) { m_Dir.x = dirx, m_Dir.y = diry; }
+	POINT GetDirect() { return m_Dir; }
+}; 
 
 // ============================================== CPlayer ============================================== 
 
@@ -31,7 +37,7 @@ public:
 	const POINT& GetCameraStartPosition() const;
 	CBullet* GetBullets();
 
-	void FireBullet();
+	void FireBullet(int mx, int my);
 
 	void UpdateCamera(const RECT& ClientRect, const RECT& MapRect);
 	virtual void Animate(float DeltaTime);
